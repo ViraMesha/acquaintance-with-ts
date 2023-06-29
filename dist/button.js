19061c35530a94fce7caeef93d32a9683b3265bf
ViraMesha <wira.mescha@gmail.com>
2023-06-29 17:19:56 +0300

    add compiler settings

---
+++ dist/button.js
@@ -0,0 +1,6 @@
+"use strict";
+const button1 = document.querySelector("#buttonId");
+button1.addEventListener("click", () => {
+    console.log("You clicked the button");
+});
+//# sourceMappingURL=button.js.map
